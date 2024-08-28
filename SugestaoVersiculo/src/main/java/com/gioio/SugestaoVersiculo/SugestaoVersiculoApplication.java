d864fe9e6e5e1667386bdac071ea33075ae40345
João Paulo <jpalves@minsait.com>
2024-08-27 21:28:38 -0300

    first commit

---
+++ SugestaoVersiculo/src/main/java/com/gioio/SugestaoVersiculo/SugestaoVersiculoApplication.java
@@ -0,0 +1,13 @@
+package com.gioio.SugestaoVersiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugestaoVersiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugestaoVersiculoApplication.class, args);
+	}
+
+}
---
+++ SugestaoVersiculo/src/test/java/com/gioio/SugestaoVersiculo/SugestaoVersiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gioio.SugestaoVersiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugestaoVersiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
